835755bb16d7770ebe71310cb713484827801a75
Jiahao Liu <jiahao.liu@careem.com>
2018-04-12 16:54:28 +0400

    Fixed a bug on the on MainActiivty regarding the layout

--- app/src/main/java/com/jiahaoliuliu/androidconcurrencywithintentservice/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/androidconcurrencywithintentservice/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.jiahaoliuliu.androidconcurrencywithintentservice
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }

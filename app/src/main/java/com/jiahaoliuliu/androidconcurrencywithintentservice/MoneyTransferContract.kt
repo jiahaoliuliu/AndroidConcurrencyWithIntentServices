51e012becf012041fcd195b5e7c2365ae7e272e4
Jiahao Liu <jiahao.liu@careem.com>
2018-04-12 16:47:28 +0400

    Created the basic version of Money transfer contract. Updated the MainActivity to Kotlin

---
+++ app/src/main/java/com/jiahaoliuliu/androidconcurrencywithintentservice/MoneyTransferContract.kt
@@ -0,0 +1,13 @@
+package com.jiahaoliuliu.androidconcurrencywithintentservice
+
+interface MoneyTransferContractView {
+
+}
+
+interface MoneyTransferContractPresenter {
+
+}
+
+interface MoneyTransferContractModel {
+
+}
